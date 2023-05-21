cf548b601924b25b30b5f1d07b0af5b3c0f76c0b
Glebosik <kalandinscoffer@gmail.com>
2023-05-21 16:13:46 +0300

    Finish Welcome, Login and Auth screens

--- lib/auth/login/view/login_page.dart
+++ lib/auth/login/view/login_page.dart
@@ -2,18 +2,19 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hackathon/auth/login/login.dart';
-import 'package:hackathon/calendar/repository/model/slot.dart';
+import 'package:hackathon/gen/colors.gen.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  static Page<void> page() => const MaterialPage<void>(child: LoginPage());
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const LoginPage());
+  }
 
   @override
   Widget build(BuildContext context) {
-    Slot slot = Slot(DateTime.now(), DateTime.now());
-    print(slot.toString());
     return Scaffold(
+      backgroundColor: ColorName.backgroundOrange,
       appBar: AppBar(title: const Text('Авторизация')),
       body: Padding(
         padding: const EdgeInsets.all(8),
